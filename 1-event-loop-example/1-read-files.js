612cc39c8ffbab2c6b774ab060758d499865c50d
trainee <trainee@zapproach.com>
2023-12-23 10:11:13 +0400

    testt

---
+++ 1-event-loop-example/1-read-files.js
@@ -0,0 +1,14 @@
+const { readFile } = require("fs");
+console.log("start a first task  ");
+
+//CHECK FILE PATH
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("Completed first task");
+});
+console.log("staring next task");
--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const _ = require("lodash");
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const items = [1, [2, [3, [4]]]];
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: "a" }
+    );
 
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("Hello person");
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .catch((result) => console.log(result))
+//   .catch((err) => console.log(err));
