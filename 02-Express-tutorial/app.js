7f8cab013a09976b7a86d0b2475ae0ea4b2a25d7
trainee <trainee@zapproach.com>
2023-12-23 16:39:47 +0400

    http

--- 02-Express-tutorial/app.js
+++ 02-Express-tutorial/app.js
@@ -1,1 +1,24 @@
-console.log("Express Tutorial");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  //home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+    // about poge
+  } else {
+    if (url === "/about") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>about page</h1>");
+      res.end();
+      //404
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>about page is not found </h1>");
+      res.end();
+    }
+  }
+});
+server.listen(5000);
