03d480a47f67a7b34c2805fc2fed857ee3d7e8c3
trainee <trainee@zapproach.com>
2023-12-25 05:12:53 +0400

    express basics

--- 02-Express-tutorial/app.js
+++ 02-Express-tutorial/app.js
@@ -1,11 +1,18 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const HomePage = readFileSync("./navbar-app/index.html");
+const HomeStyles = readFileSync("./navbar-app/styles.css");
+const HomeImage = readFileSync("./navbar-app/logo.svg");
+const HomeJavascript = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  console.log(url);
   //home page
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>home page</h1>");
+    res.write(HomePage);
     res.end();
     // about poge
   } else {
@@ -13,6 +20,22 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { "content-type": "text/html" });
       res.write("<h1>about page</h1>");
       res.end();
+      // styles
+    } else if (url === "/styles.css") {
+      res.writeHead(200, { "content-type": "text/css" });
+      res.write(HomeStyles);
+      res.end();
+      //imagw ==> logo
+    } else if (url === "/logo.svg") {
+      res.writeHead(200, { "content-type": "image/svg+xml" });
+      res.write(HomeImage);
+      res.end();
+      //javascript
+    } else if (url === "/browser-app.js") {
+      res.writeHead(200, { "content-type": "text/javascript" });
+      res.write(HomeJavascript);
+      res.end();
+
       //404
     } else {
       res.writeHead(404, { "content-type": "text/html" });
